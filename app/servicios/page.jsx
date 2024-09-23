e8765e67c3373bc5516bfa5c206489771267ff77
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-09-23 15:08:29 -0300

    asdaa

--- app/servicios/page.jsx
+++ app/servicios/page.jsx
@@ -9,47 +9,40 @@ const Servicios = () => {
     const lChevron = '<';
     const rChevron = '>';
 
-    const ServicioItem = ({ enable, color, titulo, texto }) => {
+    const ServicioItem = ({ enable, color, titulo, texto, icono }) => {
         return (
-
             <div className={` mx-4 flex flex-col ${!enable && ' opacity-30 pointer-events-none'} `}>
-                <div className={` flex-grow ${color === 'y' ? 'bg-[rgba(255,255,0,0.8)] text-black' : 'bg-[rgba(97,61,210,0.8)] text-white'} p-6 rounded-t-xl w-64 text-center `}>
-                    <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoB.src} alt='' />
+                <div className={` flex-grow ${color === 'y' ? 'bg-[rgba(255,255,0,0.8)] text-black' : 'bg-[rgba(97,61,210,0.8)] text-white'} p-6 rounded-t-xl w-80 text-center `}>
+                    <img className={` max-h-32 h-full w-auto mx-auto`} src={icono} alt='' />
                     <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>{titulo}</h3>
                     <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>{texto}</p>
                 </div>
-                <p className={` block rounded-b-xl text-center p-2 font-normal text-xl cursor-pointer  ${color === 'y' ? 'bg-[rgba(203,203,77,0.8)] text-black' : 'bg-[rgba(18,13,36,0.8)] text-white'} transform-all ease-in-out duration-300 `} href='' alt=''>Ver más</p>
+                <p className={` block rounded-b-xl text-center p-2 font-normal text-opacity-30 hover:text-opacity-100 text-lg cursor-pointer ${color === 'y' ? 'bg-[rgba(203,203,77,0.8)] text-black' : 'bg-[rgba(18,13,36,0.8)] text-white'} transform-all ease-in-out duration-300 `} href='' alt=''>Ver más</p>
             </div>
-
         )
     }
 
     const Servicios = [
-
-        {
+        {   icono: serviciosIconoC,
             color: 'p',
             titulo: 'E-Commerce',
             texto: 'Un e-commerce maximiza ventas, brinda acceso global las 24/7, mejora la experiencia del cliente y optimiza la gestión empresarial.'
         },
-
-        {
+        {   icono: serviciosIconoA,
             color: 'y',
             titulo: 'Intranet',
             texto: 'La intranet fortalece la comunicación interna, centraliza la información, facilita la colaboración y aumenta la eficiencia operativa interna de la empresa.'
         },
-
-        {
+        {   icono: serviciosIconoD,
             color: 'p',
             titulo: 'Diseño de páginas web',
             texto: 'Una página web amplía la visibilidad, establece credibilidad y facilita la interacción, impulsando la presencia y el éxito comercial.'
         },
-
-        {
+        {   icono: serviciosIconoB,
             color: 'y',
             titulo: 'Applicaciones móviles',
             texto: 'Una aplicación móvil potencia la conveniencia, personaliza la interacción, optimiza la movilidad y fortalece la conexión constante, mejorando la satisfacción del usuario.'
         }
-
     ]
 
 
@@ -83,7 +76,7 @@ const Servicios = () => {
                 <div className={` absolute top-0 left-0 w-full h-full bg-white  `} />
                 <div className={` absolute top-0 left-0 w-full h-full bg-[rgba(97,61,210,0.3)] `} />
                 <div className={` absolute top-0 left-0 w-full h-full bg-[url('/images/serviciosbg.jpg')] bg-center bg-cover opacity-10 `} />
-                <div className={` relative uppercase text-white font-Raleway font-bold pt-8 mb-4 max-w-5xl mx-auto text-center text-2xl`}>
+                <div className={` relative uppercase text-white font-RobotoCondensed font-bold pt-8 mb-4 max-w-5xl mx-auto text-center text-2xl tracking-widest `}>
                     Nuestros Servicios</div>
 
                 <div className={` relative bg-white bg-opacity-20 py-12 w-full `}>
@@ -95,6 +88,7 @@ const Servicios = () => {
                                 {Servicios.map((item, index) => (
                                     <ServicioItem
                                         enable
+                                        icono={item.icono.src}
                                         key={index}
                                         color={item.color}
                                         titulo={item.titulo}
@@ -102,8 +96,8 @@ const Servicios = () => {
                                 ))}
                             </div>
                         )}
-                        <p className={` text-white text-9xl font-light font-Economica absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{lChevron}</p>
-                        <p className={` text-white text-9xl font-light font-Economica absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{rChevron}</p>
+                        <p className={` text-white text-9xl font-extrabold font-Economica absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{lChevron}</p>
+                        <p className={` text-white text-9xl font-extrabold font-Economica absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{rChevron}</p>
                     </div>
                     {/* chevrons block end */}
 
