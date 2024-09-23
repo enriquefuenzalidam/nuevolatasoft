bfd125775e46a892e2333136223eb6a0cbae5016
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-09-23 14:55:25 -0300

    sdfsdf

--- app/servicios/page.jsx
+++ app/servicios/page.jsx
@@ -9,6 +9,50 @@ const Servicios = () => {
     const lChevron = '<';
     const rChevron = '>';
 
+    const ServicioItem = ({ enable, color, titulo, texto }) => {
+        return (
+
+            <div className={` mx-4 flex flex-col ${!enable && ' opacity-30 pointer-events-none'} `}>
+                <div className={` flex-grow ${color === 'y' ? 'bg-[rgba(255,255,0,0.8)] text-black' : 'bg-[rgba(97,61,210,0.8)] text-white'} p-6 rounded-t-xl w-64 text-center `}>
+                    <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoB.src} alt='' />
+                    <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>{titulo}</h3>
+                    <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>{texto}</p>
+                </div>
+                <p className={` block rounded-b-xl text-center p-2 font-normal text-xl cursor-pointer  ${color === 'y' ? 'bg-[rgba(203,203,77,0.8)] text-black' : 'bg-[rgba(18,13,36,0.8)] text-white'} transform-all ease-in-out duration-300 `} href='' alt=''>Ver más</p>
+            </div>
+
+        )
+    }
+
+    const Servicios = [
+
+        {
+            color: 'p',
+            titulo: 'E-Commerce',
+            texto: 'Un e-commerce maximiza ventas, brinda acceso global las 24/7, mejora la experiencia del cliente y optimiza la gestión empresarial.'
+        },
+
+        {
+            color: 'y',
+            titulo: 'Intranet',
+            texto: 'La intranet fortalece la comunicación interna, centraliza la información, facilita la colaboración y aumenta la eficiencia operativa interna de la empresa.'
+        },
+
+        {
+            color: 'p',
+            titulo: 'Diseño de páginas web',
+            texto: 'Una página web amplía la visibilidad, establece credibilidad y facilita la interacción, impulsando la presencia y el éxito comercial.'
+        },
+
+        {
+            color: 'y',
+            titulo: 'Applicaciones móviles',
+            texto: 'Una aplicación móvil potencia la conveniencia, personaliza la interacción, optimiza la movilidad y fortalece la conexión constante, mejorando la satisfacción del usuario.'
+        }
+
+    ]
+
+
     return (
         <main className={` flex-grow `}>
 
@@ -45,49 +89,21 @@ const Servicios = () => {
                 <div className={` relative bg-white bg-opacity-20 py-12 w-full `}>
 
                     {/* chevrons block */}
-                    <div className={` relative overflow-hidden flex flex-row max-w-5xl mx-auto `}>
-
-                        <div className={` mx-4 flex flex-col opacity-30 pointer-events-none `}>
-                            <div className={` flex-grow bg-[rgba(97,61,210,0.8)] p-6 text-white rounded-xl w-64 text-center `}>
-                                <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoC.src} alt='' />
-                                <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>E-Commerce</h3>
-                                <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>Un e-commerce maximiza ventas, brinda acceso global las 24/7, mejora la experiencia del cliente y optimiza la gestión empresarial.</p>
+                    <div className={` relative flex max-w-5xl mx-auto overflow-hidden items-center justify-center `}>
+                        {!!Servicios?.length && (
+                            <div className={` flex flex-row `}>
+                                {Servicios.map((item, index) => (
+                                    <ServicioItem
+                                        enable
+                                        key={index}
+                                        color={item.color}
+                                        titulo={item.titulo}
+                                        texto={item.texto} />
+                                ))}
                             </div>
-                            <p className={` block mt-2 bg-black rounded-xl text-center p-2 `} ><a className={` font-bold text-white text-base `} href='' alt=''>Ver más</a></p>
-                        </div>
-
-                        {/* these two one are enable */}
-                        <div className={` mx-4 flex flex-col `}>
-                            <div className={` flex-grow bg-[rgba(255,255,0,0.8)] p-6 text-black rounded-lg w-64 text-center `}>
-                                <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoA.src} alt='' />
-                                <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>Intranet</h3>
-                                <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>La intranet fortalece la comunicación interna, centraliza la información, facilita la colaboración y aumenta la eficiencia operativa interna de la empresa.</p>
-                            </div>
-                            <p className={` block mt-2 bg-black rounded-xl text-center p-2 `} ><a className={` font-bold text-white text-base `} href='' alt=''>Ver más</a></p>
-                        </div>
-
-                        <div className={` mx-4 flex flex-col `}>
-                            <div className={` flex-grow bg-[rgba(97,61,210,0.8)] p-6 text-white rounded-lg w-64 text-center `}>
-                                <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoD.src} alt='' />
-                                <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>Diseño de páginas web</h3>
-                                <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>Una página web amplía la visibilidad, establece credibilidad y facilita la interacción, impulsando la presencia y el éxito comercial.</p>
-                            </div>
-                            <p className={` block mt-2 bg-black rounded-xl text-center p-2 `} ><a className={` font-bold text-white text-base `} href='' alt=''>Ver más</a></p>
-                        </div>
-                        {/* enable ones end */}
-
-                        <div className={` mx-4 flex flex-col opacity-30 pointer-events-none `}>
-                            <div className={` flex-grow bg-[rgba(255,255,0,0.8)] p-6 text-black rounded-lg w-64 text-center `}>
-                                <img className={` max-h-32 h-full w-auto mx-auto`} src={serviciosIconoB.src} alt='' />
-                                <h3 className={` font-RobotoCondensed font-bold text-xl text-center my-3 `}>Applicaciones móviles</h3>
-                                <p className={` font-normal text-base leading-snug indent-4 text-justify hyphens-auto font-Raleway `}>Una aplicación móvil potencia la conveniencia, personaliza la interacción, optimiza la movilidad y fortalece la conexión constante, mejorando la satisfacción del usuario.</p>
-                            </div>
-                            <p className={` block mt-2 bg-black rounded-xl text-center p-2 `} ><a className={` font-bold text-white text-base `} href='' alt=''>Ver más</a></p>
-                        </div>
-
+                        )}
                         <p className={` text-white text-9xl font-light font-Economica absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{lChevron}</p>
                         <p className={` text-white text-9xl font-light font-Economica absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer `}>{rChevron}</p>
-
                     </div>
                     {/* chevrons block end */}
 
