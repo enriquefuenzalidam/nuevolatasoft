b1389d3afb05a1810cf97038b524d6bd52c219a7
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2024-10-08 10:54:20 -0300

    servicios card

--- app/servicios/ServicioCard.jsx
+++ app/servicios/ServicioCard.jsx
@@ -24,9 +24,9 @@ const ServicioCard = ({ id, index, titulo, precio, cuotas, descripcion, imagen, 
         if (titulo === "Proveedores para agencias de marketing") {
             return (
                 <p className={`block mt-7`}>
-                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`} 
-                       href='/contactar' alt='Contactar'>
-                       Contactar
+                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`}
+                        href='/contactar' alt='Contactar'>
+                        Contactar
                     </a>
                 </p>
             );
@@ -34,15 +34,15 @@ const ServicioCard = ({ id, index, titulo, precio, cuotas, descripcion, imagen, 
             return (
                 <>
                     <p className={`block mt-7`}>
-                        <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`} 
-                           href='mailto:latasoftweb@gmail.com' alt='Vender o suscribir mi software' target="_blank" rel="noopener noreferrer">
-                           Vender o suscribir mi software
+                        <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`}
+                            href='mailto:latasoftweb@gmail.com' alt='Vender o suscribir mi software' target="_blank" rel="noopener noreferrer">
+                            Vender o suscribir mi software
                         </a>
                     </p>
                     <p className={`block mt-3`}>
-                        <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#EDE8FB] border-solid border-2 border-[#613DD2] border-opacity-10 rounded-full text-[#613DD2]`} 
-                           href='https://softwareya.cl/' alt='Comprar o suscribir a un software' target="_blank" rel="noopener noreferrer">
-                           Comprar o suscribir a un software
+                        <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#EDE8FB] border-solid border-2 border-[#613DD2] border-opacity-10 rounded-full text-[#613DD2]`}
+                            href='https://softwareya.cl/' alt='Comprar o suscribir a un software' target="_blank" rel="noopener noreferrer">
+                            Comprar o suscribir a un software
                         </a>
                     </p>
                 </>
@@ -50,18 +50,18 @@ const ServicioCard = ({ id, index, titulo, precio, cuotas, descripcion, imagen, 
         } else if (titulo === "Venta de base de datos") {
             return (
                 <p className={`block mt-7`}>
-                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`} 
-                       href='https://impulsoventa.netlify.app/' alt='Comprar base de datos' target="_blank" rel="noopener noreferrer">
-                       Comprar base de datos
+                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`}
+                        href='https://impulsoventa.netlify.app/' alt='Comprar base de datos' target="_blank" rel="noopener noreferrer">
+                        Comprar base de datos
                     </a>
                 </p>
             );
         } else {
             return (
                 <p className={`block mt-7`}>
-                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`} 
-                       href='mailto:latasoftweb@gmail.com' alt=''>
-                       Cotizar
+                    <a className={`block font-medium text-base md:text-lg text-center py-1 bg-[#613DD2] border-solid border-2 border-[#613DD2] rounded-full text-white`}
+                        href='mailto:latasoftweb@gmail.com' alt=''>
+                        Cotizar
                     </a>
                 </p>
             );
@@ -80,10 +80,10 @@ const ServicioCard = ({ id, index, titulo, precio, cuotas, descripcion, imagen, 
                 <div className={`absolute top-0 left-0 w-full h-full backdrop-grayscale-60`} />
 
                 <div id={id} className={`relative pb-32 pt-40 w-full`}>
-                    <div className={`relative flex max-w-5xl mx-auto items-center justify-center transition-all ease-in-out duration-300`}>
-                        <div data-aos-once="true" data-aos='fade' className={` z-40 relative rounded-md font-Raleway p-5 w-80 md:w-[42rem] transition-all ease-in-out duration-300 bg-white bg-opacity-60`}>
+                    <div className={`relative flex max-w-5xl mx-auto items-center justify-center `}>
+                        <div className={` z-40 relative rounded-md font-Raleway p-5 w-80 md:w-[42rem] bg-white bg-opacity-60`}>
 
-                            <div className={`flex flex-col-reverse md:flex-row transition-all ease-in-out duration-300 justify-center items-center px-0 pt-0 md:px-3 md:pt-3`}>
+                            <div className={`flex flex-col-reverse md:flex-row justify-center items-center px-0 pt-0 md:px-3 md:pt-3`}>
                                 <div className={`w-full md:w-7/12 transition-all ease-in-out duration-300`}>
                                     <h3 className={`block text-2xl md:text-3xl font-medium text-black pr-5 text-left`}>{titulo}</h3>
                                     <div className={`mt-3 pr-6`}>
@@ -100,32 +100,30 @@ const ServicioCard = ({ id, index, titulo, precio, cuotas, descripcion, imagen, 
                             <div className={`block mt-7 `}>
                                 {renderButtons()}
 
-                                <div
-                                    className={`block mt-3 cursor-pointer`}
-                                    onClick={toggleDescriptionVisibility} >
+                                <div className={`block mt-3`} >
 
-                                    {/* is it possible to get the scrollHeight of this element, to put it instead of "h-auto" */}
-                                    <div    ref={descriptionRef}
-                                            className={` transition-all ease-in-out duration-300 py-1 bg-[#EDE8FB] border-solid border-2 border-[#613DD2] border-opacity-10 rounded-3xl overflow-hidden `}>
-                                    <p className={` font-medium text-base md:text-lg text-center text-[#613DD2] `}>
-                                        {isDescriptionVisible ? 'Ocultar lo que incluye ' : 'Ver lo que incluye '}
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 512 512"
-                                            fill="currentColor"
-                                            className={`inline w-4 h-auto ml-1 transition-transform duration-300 ${isDescriptionVisible ? 'rotate-180 align-middle ' : 'rotate-0 align-middle '}`}
-                                        >
-                                            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                                        </svg>
-                                    </p>
-                                    {!!descripcion && (
-                                    <ul className={` ${isDescriptionVisible ? 'opacity-100' : ' opacity-0 ' } mx-12 transition-all ease-in-out duration-300 font-medium text-base md:text-lg text-center text-[#613DD2] `}>
-                                            {descripcion.split('. ').map((line, index) => (
-                                                <li key={index} className={` list-disc list-outside my-5 opacity-80 text-justify hyphens-auto  `}>
-                                                    {line}.
-                                                </li>
-                                            ))}
-                                        </ul>
+                                    <div ref={descriptionRef}
+                                        className={` transition-all ease-in-out duration-300 py-1 bg-[#EDE8FB] border-solid border-2 border-[#613DD2] border-opacity-10 rounded-3xl overflow-hidden `}>
+                                        <p className={` font-medium text-base md:text-lg text-center text-[#613DD2] `}>
+                                            <span className={` cursor-pointer `} onClick={toggleDescriptionVisibility}>
+                                                {isDescriptionVisible ? 'Ocultar lo que incluye ' : 'Ver lo que incluye '}
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                    fill="currentColor"
+                                                    className={`inline w-4 h-auto ml-1 transition-transform duration-300 ${isDescriptionVisible ? 'rotate-180 align-middle ' : 'rotate-0 align-middle '}`} >
+                                                    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                                                </svg>
+                                            </span>
+                                        </p>
+                                        {!!descripcion && (
+                                            <ul className={` ${isDescriptionVisible ? 'opacity-100' : ' opacity-0 '} mx-12 transition-all ease-in-out duration-300 font-medium text-base md:text-lg text-center text-[#613DD2] `}>
+                                                {descripcion.split('. ').map((line, index) => (
+                                                    <li key={index} className={` list-disc list-outside my-5 opacity-80 text-justify hyphens-auto  `}>
+                                                        {line}.
+                                                    </li>
+                                                ))}
+                                            </ul>
                                         )}
                                     </div>
                                 </div>
